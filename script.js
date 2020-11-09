10c6688fdccb245c27932d28cc2406f8b55f7be3
James Flood <floodadosious111@gmail.com>
2020-11-08 22:21:35 -0500

    updated media queries and added JS for contact form

--- script.js
+++ script.js
@@ -1,18 +1,82 @@
 /* toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function() {
+  fields.firstName = document.getElementById('firstName');
+  fields.lastName = document.getElementById('lastName');
+  fields.email = document.getElementById('email');
+  fields.phone = document.getElementById('phone');
+ })
+
+ function isNotEmpty(value) {
+  if (value == null || typeof value == 'undefined' ) return false;
+  return (value.length > 0);
+ }
+
+ function isNumber(num) {
+  return (num.length > 0) && !isNaN(num);
+ }
+
+ function isEmail(email) {
+   let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return regex.test(String(email).toLowerCase());
+ }
+
+ function fieldValidation(field, validationFunction) {
+  if (field == null) return false;
+ 
+  let isFieldValid = validationFunction(field.value)
+  if (!isFieldValid) {
+  field.className = 'placeholderRed';
+  } else {
+  field.className = '';
+  }
+ 
+  return isFieldValid;
+ }
+
+ function isValid() {
+  var valid = true;
+  
+  valid &= fieldValidation(fields.firstName, isNotEmpty);
+  valid &= fieldValidation(fields.lastName, isNotEmpty);
+  valid &= fieldValidation(fields.email, isEmail);
+  return valid;
+ }
+
+ class User {
+  constructor(firstName, lastName, email) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
+  }
+ }
+
+function submitForm() {
+  if (isValid()) {
+    let usr = new User(firstName.value, lastName.value, email.value);
+
+    alert(`${usr.firstName} thanks for submitting.`)
+
+  } else {
+    alert("There was an error!")
   }
 }
+
